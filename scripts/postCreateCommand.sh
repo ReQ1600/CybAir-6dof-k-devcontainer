7abd0ae81949f7964f51fe23a69aa4ccb62faf69
Mateusz Drogowski <drogowskimateusz8@gmail.com>
2025-02-21 17:38:46 +0000

    fixed bashrc command appending, added rosdep setup

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,7 +1,9 @@
 sudo apt install ros-humble-moveit -y &&\
 sudo apt install ros-humble-rmw-cyclonedds-cpp -y &&\
 echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ~/.bashrc &&\
-sudo rosdep update &&\
+sudo rosdep init &&\
+rosdep update &&\
 sudo apt update &&\
+sudo apt dist-upgrade &&\
 sudo rosdep install --from-paths src --ignore-src -y -r &&\
 sudo chown -R $(whoami) /home/ws/
